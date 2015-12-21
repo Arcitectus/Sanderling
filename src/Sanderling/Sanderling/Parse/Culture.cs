355da1b578613e1de274613eb95d0accd1ee8662
Arcitectus <michael@xn--rtzel-gra.com>
2015-12-21 21:22:09 +0100

    move culture depended code to dedicated file. add test for collision in key UI text mapping.

---
+++ src/Sanderling/Sanderling/Parse/Culture.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WindowsInput.Native;
+
+namespace Sanderling.Parse
+{
+	static public class CultureAggregated
+	{
+		static VirtualKeyCode CtrlKeyCode => Extension.CtrlKeyCode;
+		static VirtualKeyCode AltKeyCode => Extension.AltKeyCode;
+
+		static readonly KeyValuePair<string, VirtualKeyCode>[] SetKeyFCodeFromUIText =
+			Enumerable.Range(0, 12).Select(Index => new KeyValuePair<string, VirtualKeyCode>("f" + (Index + 1), VirtualKeyCode.F1 + Index)).ToArray();
+
+		static public readonly KeyValuePair<string, VirtualKeyCode>[] SetKeyCodeFromUIText = new[]
+		{
+			new KeyValuePair<string, VirtualKeyCode>("ctrl", CtrlKeyCode),
+			new KeyValuePair<string, VirtualKeyCode>("alt", AltKeyCode),
+			new KeyValuePair<string, VirtualKeyCode>("shift", VirtualKeyCode.SHIFT),
+			new KeyValuePair<string, VirtualKeyCode>("enter", VirtualKeyCode.RETURN),
+			new KeyValuePair<string, VirtualKeyCode>("space", VirtualKeyCode.SPACE),
+
+			//	"de"
+			new KeyValuePair<string, VirtualKeyCode>("strg", CtrlKeyCode),
+			new KeyValuePair<string, VirtualKeyCode>("umschalt", VirtualKeyCode.SHIFT),
+			new KeyValuePair<string, VirtualKeyCode>("eingabe", VirtualKeyCode.RETURN),
+			new KeyValuePair<string, VirtualKeyCode>("leer", VirtualKeyCode.SPACE),
+
+			//	TODO: Insert missing mappings here.
+
+		}.Concat(SetKeyFCodeFromUIText).ToArray();
+
+		static public IEnumerable<string> KeyCodeFromUITextSetCollidingKey() =>
+			SetKeyCodeFromUIText?.GroupBy(KeyCodeFromUIText => KeyCodeFromUIText.Key?.ToLower())?.Where(Group => 1 < Group.Count())?.Select(Group => Group.Key);
+	}
+}
--- src/Sanderling/Sanderling/Parse/Extension.cs
+++ src/Sanderling/Sanderling/Parse/Extension.cs
@@ -18,32 +18,11 @@ namespace Sanderling.Parse
 		static public INeocom Parse(this MemoryStruct.INeocom Neocom) =>
 			null == Neocom ? null : new Neocom(Neocom);
 
-		const VirtualKeyCode AltKeyCode = VirtualKeyCode.LMENU;
-		const VirtualKeyCode CtrlKeyCode = VirtualKeyCode.CONTROL;
-
-		static readonly KeyValuePair<string, VirtualKeyCode>[] SetKeyFCodeFromUIText =
-			Enumerable.Range(0, 12).Select(Index => new KeyValuePair<string, VirtualKeyCode>("f" + (Index + 1), VirtualKeyCode.F1 + Index)).ToArray();
-
-		static readonly KeyValuePair<string, VirtualKeyCode>[] SetKeyCodeFromUIText = new[]
-		{
-			new KeyValuePair<string, VirtualKeyCode>("ctrl", CtrlKeyCode),
-			new KeyValuePair<string, VirtualKeyCode>("alt", AltKeyCode),
-			new KeyValuePair<string, VirtualKeyCode>("shift", VirtualKeyCode.SHIFT),
-			new KeyValuePair<string, VirtualKeyCode>("enter", VirtualKeyCode.RETURN),
-			new KeyValuePair<string, VirtualKeyCode>("space", VirtualKeyCode.SPACE),
-
-			//	"de"
-			new KeyValuePair<string, VirtualKeyCode>("strg", CtrlKeyCode),
-			new KeyValuePair<string, VirtualKeyCode>("umschalt", VirtualKeyCode.SHIFT),
-			new KeyValuePair<string, VirtualKeyCode>("eingabe", VirtualKeyCode.RETURN),
-			new KeyValuePair<string, VirtualKeyCode>("leer", VirtualKeyCode.SPACE),
-
-			//	TODO: Insert missing mappings here.
-
-		}.Concat(SetKeyFCodeFromUIText).ToArray();
+		public const VirtualKeyCode AltKeyCode = VirtualKeyCode.LMENU;
+		public const VirtualKeyCode CtrlKeyCode = VirtualKeyCode.CONTROL;
 
 		static VirtualKeyCode? KeyCodeFromUIText(this string KeyUIText) =>
-			SetKeyCodeFromUIText?.CastToNullable()?.FirstOrDefault(UITextAndKeyCode =>
+			CultureAggregated.SetKeyCodeFromUIText?.CastToNullable()?.FirstOrDefault(UITextAndKeyCode =>
 			string.Equals(UITextAndKeyCode?.Key, KeyUIText, System.StringComparison.OrdinalIgnoreCase))?.Value;
 
 		static public IEnumerable<VirtualKeyCode> ListKeyCodeFromUIText(this string ListKeyUITextAggregated)
