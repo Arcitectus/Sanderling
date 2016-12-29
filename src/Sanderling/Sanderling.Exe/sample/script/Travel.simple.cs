f68bfeaa6a1fe6ff4932664e77aaaa3ab56626de
Anton <toshegg@gmail.com>
2016-12-28 21:18:41 -0800

    slight improvement of autopilot - undock support - stoping script on arriving to station

--- src/Sanderling/Sanderling.Exe/sample/script/Travel.simple.cs
+++ src/Sanderling/Sanderling.Exe/sample/script/Travel.simple.cs
@@ -1,9 +1,40 @@
-﻿//	This is a warp to 0km auto-pilot, making your travels faster and thus safer by directly warping to gates/stations.
+//	This is a warp to 0km auto-pilot, making your travels faster and thus safer by directly warping to gates/stations.
 
-while(true)
+var Measurement = Sanderling?.MemoryMeasurementParsed?.Value;
+var fly = true;
+var arriving = false;
+
+void Undock()
 {
-	var Measurement = Sanderling?.MemoryMeasurementParsed?.Value;
+	while(Measurement?.IsDocked ?? false)
+	{
+		var undockBtnText = Measurement?.WindowStation?.FirstOrDefault()?.LabelText.FirstOrDefault(candidate =>
+				candidate.Text.Contains("Undock"))?.Text;
+		if (!undockBtnText.Contains("Abort")) {
+			Sanderling.MouseClickLeft(Measurement?.WindowStation?.FirstOrDefault()?.UndockButton);
+		}
+		Host.Log("waiting for undocking to complete.");
+		Host.Delay(8000);
+		Sanderling.InvalidateMeasurement();
+		Host.Delay(1000);
+		Measurement = Sanderling?.MemoryMeasurementParsed?.Value;
+	}
+}
 
+while(fly)
+{
+	Measurement = Sanderling?.MemoryMeasurementParsed?.Value;
+	if (Measurement?.IsDocked ?? false)
+	{
+		if (arriving) {
+			fly = false;
+			Host.Log("Arrived, stopping here");
+			goto loop;
+		} else {
+			Undock();
+		}
+	}
+	
 	var ManeuverType = Measurement?.ShipUi?.Indication?.ManeuverType;
 
 	if(ShipManeuverTypeEnum.Warp == ManeuverType	||
@@ -41,6 +72,10 @@ while(true)
 
 	Host.Log("menu entry found. clicking to initiate warp.");
 	Sanderling.MouseClickLeft(MenuEntry);
+	if (MenuEntry.Text.RegexMatchSuccessIgnoreCase("dock")) {
+		Host.Log("Warping to Dock, getting ready to stop");
+		arriving = true;
+	}
 
 loop:
 	//	wait for four seconds before repeating.
@@ -48,4 +83,3 @@ loop:
 	//	make sure new measurement will be taken.
 	Sanderling.InvalidateMeasurement();
 }
-
