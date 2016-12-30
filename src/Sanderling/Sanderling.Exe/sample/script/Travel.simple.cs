0d9bf3b097749951e2a15d5cc97e9b04eb32900b
Anton <toshegg@gmail.com>
2016-12-29 23:01:57 -0800

    Changes requested on pull-request

--- src/Sanderling/Sanderling.Exe/sample/script/Travel.simple.cs
+++ src/Sanderling/Sanderling.Exe/sample/script/Travel.simple.cs
@@ -1,40 +1,9 @@
 //	This is a warp to 0km auto-pilot, making your travels faster and thus safer by directly warping to gates/stations.
 
-var Measurement = Sanderling?.MemoryMeasurementParsed?.Value;
-var fly = true;
-var arriving = false;
-
-void Undock()
+while(true)
 {
-	while(Measurement?.IsDocked ?? false)
-	{
-		var undockBtnText = Measurement?.WindowStation?.FirstOrDefault()?.LabelText.FirstOrDefault(candidate =>
-				candidate.Text.Contains("Undock"))?.Text;
-		if (!undockBtnText.Contains("Abort")) {
-			Sanderling.MouseClickLeft(Measurement?.WindowStation?.FirstOrDefault()?.UndockButton);
-		}
-		Host.Log("waiting for undocking to complete.");
-		Host.Delay(8000);
-		Sanderling.InvalidateMeasurement();
-		Host.Delay(1000);
-		Measurement = Sanderling?.MemoryMeasurementParsed?.Value;
-	}
-}
+	var Measurement = Sanderling?.MemoryMeasurementParsed?.Value;
 
-while(fly)
-{
-	Measurement = Sanderling?.MemoryMeasurementParsed?.Value;
-	if (Measurement?.IsDocked ?? false)
-	{
-		if (arriving) {
-			fly = false;
-			Host.Log("Arrived, stopping here");
-			goto loop;
-		} else {
-			Undock();
-		}
-	}
-	
 	var ManeuverType = Measurement?.ShipUi?.Indication?.ManeuverType;
 
 	if(ShipManeuverTypeEnum.Warp == ManeuverType	||
@@ -72,10 +41,6 @@ while(fly)
 
 	Host.Log("menu entry found. clicking to initiate warp.");
 	Sanderling.MouseClickLeft(MenuEntry);
-	if (MenuEntry.Text.RegexMatchSuccessIgnoreCase("dock")) {
-		Host.Log("Warping to Dock, getting ready to stop");
-		arriving = true;
-	}
 
 loop:
 	//	wait for four seconds before repeating.
@@ -83,3 +48,4 @@ loop:
 	//	make sure new measurement will be taken.
 	Sanderling.InvalidateMeasurement();
 }
+
