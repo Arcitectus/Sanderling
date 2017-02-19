243caf9d58bc2de7f29bdbd5156af065f80fdb70
Michael Rätzel <Viir@Viir.de>
2017-02-19 13:00:39 +0100

    Release Sanderling version 17.01.18: Switch version 17-01-18 of eve online memory reading.

--- src/Sanderling/Sanderling/ExeConfig.cs
+++ src/Sanderling/Sanderling/ExeConfig.cs
@@ -3,7 +3,7 @@
 	public class ExeConfig
 	{
 		public const string ConfigLicenseKeyDefault = "Sanderling.Free";
-		public const string ConfigServiceId = "Sanderling.16-11-17";
+		public const string ConfigServiceId = "Sanderling.17-01-18";
 		public const string ConfigApiVersionAddressDefault = @"http://service.botengine.de:4074/api";
 
 		public const string EveOnlineProcessMainModuleFileName = "ExeFile.exe";
