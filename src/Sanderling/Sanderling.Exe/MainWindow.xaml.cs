a47828f25ada760eff8b9c15130e731e5f677d15
ML <ML@DESKTOP-55FVGS3>
2017-05-21 04:44:53 -0700

    Added main window state persister - will save size and position of the main window upon exit and restore upon next start

--- src/Sanderling/Sanderling.Exe/MainWindow.xaml.cs
+++ src/Sanderling/Sanderling.Exe/MainWindow.xaml.cs
@@ -10,7 +10,13 @@ namespace Sanderling.Exe
 		public MainWindow()
 		{
 			InitializeComponent();
-		}
+
+            State.Tracker.Configure(this)//the object to track
+                .IdentifyAs("Main Sanderling Window")//a string by which to identify the target object
+                .AddProperties<Window>(w => w.Height, w => w.Width, w => w.Top, w => w.Left, w => w.WindowState)//properties to track
+                .RegisterPersistTrigger(nameof(SizeChanged))//when to persist data to the store
+                .Apply();//apply any previously stored data
+        }
 
 		public string TitleComputed =>
 			"Sanderling v" + (TryFindResource("AppVersionId") ?? "");
