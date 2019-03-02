3282b4e0d545be6c5d8d230392a28eef22013776
Michael Rätzel <Viir@Viir.de>
2019-03-02 20:28:51 +0000

    Improve readability and update link

--- src/Sanderling/Sanderling.MemoryReading.Test/MemoryReadingDemo.cs
+++ src/Sanderling/Sanderling.MemoryReading.Test/MemoryReadingDemo.cs
@@ -10,28 +10,28 @@ namespace Sanderling.MemoryReading.Test
 	public class MemoryReadingDemo
 	{
 		[Test]
-		[Explicit("Do not include this method when running all tests. The only reason this is marked as test is to simplify execution from Visual Studio UI.")]
+		[Explicit("Do not include this method when running all tests. The only reason this is marked as a `Test` is to simplify execution from Visual Studio UI.")]
 		public void Demo_memory_reading_from_process_sample()
 		{
-			//	To obtain a process sample of the eve online client to use with this reading approach,
-			//	see the guide at http://forum.botengine.org/t/collecting-samples-for-memory-reading-development/50
+			//	To get a process sample of the EVE Online client to use with this reading approach,
+			//	see the guide at https://forum.botengine.org/t/how-to-collect-samples-for-memory-reading-development/50
 
-			var sampleFilePath =
+			var windowsProcessMeasurementFilePath =
 				Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
 				.PathToFilesysChild("Sanderling.Process.Sample")
 				.PathToFilesysChild("my-eve-online-client-process-sample.zip");
 
-			var snapshotZipArchiv = Bib3.Glob.InhaltAusDataiMitPfaad(sampleFilePath);
+			var windowsProcessMeasurementZipArchive = System.IO.File.ReadAllBytes(windowsProcessMeasurementFilePath);
 
-			var snapshot = BotEngine.Interface.Process.Snapshot.Extension.SnapshotFromZipArchive(snapshotZipArchiv);
+			var windowsProcessMeasurement = BotEngine.Interface.Process.Snapshot.Extension.SnapshotFromZipArchive(windowsProcessMeasurementZipArchive);
 
-			var memoryReader = new BotEngine.Interface.Process.Snapshot.SnapshotReader(snapshot?.ProcessSnapshot?.MemoryBaseAddressAndListOctet);
+			var memoryReader = new BotEngine.Interface.Process.Snapshot.SnapshotReader(windowsProcessMeasurement?.ProcessSnapshot?.MemoryBaseAddressAndListOctet);
 
-			var memoryMeasurement = memoryReader.MemoryMeasurement().Parse();
+			var parsedMemoryMeasurement = memoryReader.MemoryMeasurement().Parse();
 
-			//	At this point, you will find in the "memoryMeasurement" variable the contents read from the process sample as they would appear in the Sanderling API Explorer.
+			//	At this point, you will find in the "parsedMemoryMeasurement" variable the contents read from the process measurement as they would appear in the Sanderling API Explorer.
 
-			Console.WriteLine("Overview window read: " + (memoryMeasurement.WindowOverview?.Any() ?? false));
+			Console.WriteLine("Overview window read: " + (parsedMemoryMeasurement.WindowOverview?.Any() ?? false));
 		}
 	}
 }
