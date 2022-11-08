12a54be74d1063018647c2e3d65f6ada9f895ad5
Michael Rätzel <Viir@Viir.de>
2022-11-08 21:33:52 +0000

    Integrate training data from Abaddons Korean game client
    Adapt to the session `session-2022-05-26T03-13-42-83df2b.zip` shared by Abaddon at https://forum.botlab.org/t/i-want-to-add-korean-support-on-eve-online-bot-what-should-i-do/4370/14
    
    Adapt the bot's behavior for multiple events in session-2022-05-26T03-13-42-83df2b:
    
    + Adapt bot behavior for event 75: Do not send any input. (If the ship UI contains the text `점프 중`, assume the ship is jumping.)
    + Adapt bot behavior for event 90: Click on menu entry with label `도킹`. (Assume we can cause the ship to dock with the menu entry labeled `도킹`.)

--- implement/alternate-ui/source/src/EveOnline/ParseUserInterface.elm
+++ implement/alternate-ui/source/src/EveOnline/ParseUserInterface.elm
@@ -1091,6 +1091,12 @@ parseShipUIIndication indicationUINode =
             , ( "Jump", ManeuverJump )
             , ( "Orbit", ManeuverOrbit )
             , ( "Approach", ManeuverApproach )
+
+            -- Sample `session-2022-05-23T23-00-32-87ba97.zip` shared by Abaddon at https://forum.botlab.org/t/i-want-to-add-korean-support-on-eve-online-bot-what-should-i-do/4370/9
+            , ( "워프 드라이브 가동", ManeuverWarp )
+
+            -- Sample `session-2022-05-26T03-13-42-83df2b.zip` shared by Abaddon at https://forum.botlab.org/t/i-want-to-add-korean-support-on-eve-online-bot-what-should-i-do/4370/14
+            , ( "점프 중", ManeuverJump )
             ]
                 |> List.filterMap
                     (\( pattern, candidateManeuverType ) ->
